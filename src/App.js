d720c236aa4e626448a0509072c3c11c21b54789
mb2o <mboomaars@gmail.com>
2019-07-06 20:49:51 +0200

    Navbar component logic

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+
+import Navbar from './components/Navbar/Navbar';
+
 import './App.scss';
 
 function App() {
-  return <h1>Our current app</h1>;
+  return (
+    <Fragment>
+      <Navbar />
+      <h1>Our current app</h1>
+    </Fragment>
+  );
 }
 
 export default App;
